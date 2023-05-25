f3500b60138f38fa27eb029e4ca0c0164b62184d
Jelvys Triana <jelvysc@gmail.com>
2023-05-25 16:32:13 -0400

    cmd look

---
+++ locations.py
@@ -0,0 +1,18 @@
+# Localizaciones del Juego the Lost City
+
+#Area 1: La Isla
+
+locations_area_1 = [
+    {
+        'name':'Playa',
+        'descripcion':'Una hermosa playa de arena blanca.',
+        'items':['concha', 'palo', 'botella de agua'],
+        'required_level':1
+    },
+    {
+        'name':'Acantilado',
+        'descripcion':'Un cantilado tranquilo y rocoso.',
+        'items':['anillo', 'espada oxidada', 'botas de cuero'],
+        'required_level':1
+    }
+]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import keep_alive as alive
 import constants as key
 from telebot import *
+import locations as area
 
 bot = telebot.TeleBot(key.API_KEY)
 
@@ -20,6 +21,10 @@ def cmd_help(message):
     /take - Recoger articulos 
     /use - Usar articulos del inventario    
     """)
+
+@bot.message_handler(commands=['look', '.look', 'LOOK'])
+def cmd_look(message)
+    bot.reply_to(message, f"Estas en {area.locations_area_1['name']}.\n\n {area.locations_area_1['descripcion']}")
 
 
 if __name__ == '__main__':
