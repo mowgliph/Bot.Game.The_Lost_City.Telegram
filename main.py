dc3a9ec674e19524087947fcb8f6bc200c54bc4a
Jelvys Triana <jelvysc@gmail.com>
2023-05-25 14:32:28 -0400

    otras pruebas

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ from telegram.ext import Updater, CommandHandler
 print("Loading The Lost City...");
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text = """
+    update.message.reply_tex("""
     Welcome to the Lost City adventure game! \n
     Type /help to see the available commands. 
     """)
 
-updater = Updater(API_KEY, use_context=True)
-dispatcher = updater.dispatcher
+def main():
+    updater = Updater(API_KEY, use_context=True)
+    dp = updater.dispatcher
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+    #commands area
+    dp.add_handler(CommandHandler("start", start))
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
+
+main()
