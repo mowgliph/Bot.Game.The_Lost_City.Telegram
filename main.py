6c55c36755ac44fecba852451d291514931c330e
Jelvys Triana <jelvysc@gmail.com>
2023-05-25 16:02:22 -0400

    update cmds

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ bot = telebot.TeleBot(key.API_KEY)
 def cmd_start(message):
     bot.reply_to(message, "Welcome to the Lost City adventure game! \n Type /help to see the available commands.")
 
+@bot.message_handler(commands=['help'])
+def cmd_help(message):
+    bot.reply_to(message, """
+    A continuacion todos los comandos disponibles en el Juego: \n     
+    /start - Inicia el Juego 
+    /help - Muestra lista de los comandos 
+    /look - Explora tu entorno 
+    /go - Muevete a una nueva ubicacion 
+    /inventory - Abre el Inventario 
+    /take - Recoger articulos 
+    /use - Usar articulos del inventario    
+    """)
+
+
 if __name__ == '__main__':
     print("Loading The Lost City")
     bot.infinity_polling()
